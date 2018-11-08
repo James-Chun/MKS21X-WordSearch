fd99fa0d8fa53f58c9eff6f8b9d1b52268aa30cd
James-Chun <jchun00@stuy.edu>
2018-11-07 23:37:33 -0500

    added Vertical

--- Driver.java
+++ Driver.java
@@ -13,7 +13,7 @@ public class Driver{
     n.addWordHorizontal("hi",0,0);
     System.out.println(n);
 
-    System.out.println("~ ~ These Tests See If Nothing Happens When (Mismatched) OverLapping Words Occur Or When Desired Word Is Too Large ~ ~");
+    System.out.println("\n~ ~ These Tests See If Nothing Happens When (Mismatched) OverLapping Words Occur Or When Desired Word Is Too Large ~ ~");
     System.out.println("~ Should Return 2 True and 2 False and Grid with Just \"hin\" and \"X\" ~");
     System.out.println(n.addWordHorizontal("X",2,0));
     System.out.println(n.addWordHorizontal("in",0,1));
@@ -26,5 +26,11 @@ public class Driver{
     System.out.println("\n~ Should print \"food\" in second column");
     n2.addWordVertical("food",0,1);
     System.out.print(n2);
+
+    System.out.println("\n~ ~ These Tests See If Nothing Happens When (Mismatched) OverLapping Words Occur Or When Desired Word Is Too Large ~ ~");
+    System.out.println("~ Should Return False then True and Grid with Just \"foody\" ~");
+    System.out.println(n2.addWordVertical("fe",2,1));
+    System.out.println(n2.addWordVertical("dy",3,1));
+    System.out.println("\n"+n2);
   }
 }
